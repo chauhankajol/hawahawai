fd7180e010678ab85b8805fe8a0ff2286e4b1fc2
kajol <kajolchauhan450@gmail.com>
2024-07-03 13:21:21 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base :'/hawahawai/',
   plugins: [react()],
-  base :'/haww-hawai/'
+  
 })
